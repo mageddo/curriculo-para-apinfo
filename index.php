dcd7fa42d31154732a4da1d13559f49cd0bf1ca3
Elvis <edigitalb@gmail.com>
2015-10-11 13:11:39 -0300

    melhorando a experiencia do usuário com o layout

--- index.php
+++ index.php
@@ -24,11 +24,35 @@
 
 	  </div>
 	</div>
-	<div class="areaConversao">
-		<div class="container">
-			<h1>Converta seu currículo do word, LibreOffice, etc. para a APInfo</h1><br/>
-		</div>
-		<div class=" clearfix">
+	<div class="areaConversao col-xs-12">
+		<h1>Converta seu currículo do word, LibreOffice, etc. para a APInfo</h1><br />
+		<ul class="nav nav-pills navbar-default ">
+		  <li role="presentation" class="">
+		  	<button 
+		  		class="btn btn-success btnConverter" 
+					title="Pega o HTML do editor, converte para o modelo da APInfo e joga no campo de resultado "
+				>
+					<i class="glyphicon glyphicon-refresh"></i>
+					converter
+				</button>
+		  </li>
+		  <li>
+		  	<button class="btn btn-default btnPreview" title="Abre modal para previsualizar o resultado" >
+					<i class="glyphicon glyphicon-sunglasses"></i>
+					preview
+				</button>
+		  </li>
+		  <li>
+		  	<button 
+				class="btn btn-default btnVoltarEditor" 
+				title="Pega o HTML do campo de resultado e joga no editor para melhorias"
+				>
+					<i class="glyphicon glyphicon-backward"></i>
+					voltar do código para o editor
+				</button>
+		  </li>
+		</ul><br />
+		<div class="row clearfix">
 			<div class="col-xs-6">
 				<label for="html">Cole do word, writer, etc.</label>
 				<textarea  id="html" name="html"></textarea>
@@ -39,26 +63,8 @@
 				<textarea class="well well-lg form-control" id="convertedHtml" name="convertedHtml"></textarea>
 			</div>
 			<div class="clearfix visible-xs-block"></div>
-		</div><br/>
-		<div class="col-xs-12">
-			<button class="btn btn-success btnConverter" 
-				title="Pega o HTML do editor, converte para o modelo da APInfo e joga no campo de resultado "
-			>
-				<i class="glyphicon glyphicon-refresh"></i>
-				converter
-			</button>
-			<button class="btn btn-default btnPreview" title="Abre modal para previsualizar o resultado" >
-				<i class="glyphicon glyphicon-sunglasses"></i>
-				preview
-				</button>
-			<button 
-				class="btn btn-default btnVoltarEditor" 
-				title="Pega o HTML do campo de resultado e joga no editor para melhorias"
-				>
-					<i class="glyphicon glyphicon-backward"></i>
-					voltar do código para o editor
-				</button>
 		</div>
+	</div>
 	<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 	<script src="http://tinymce.cachefly.net/4.0/tinymce.min.js"></script>
